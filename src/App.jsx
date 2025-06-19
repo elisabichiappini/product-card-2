78c80a1ffadc14561a5283ed106dfe8d62c6881c
Elisa Chiappini <elisabichiappini@gmail.com>
2025-06-19 17:36:44 +0200

    feat: add app configurazione

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
-
 import './App.css';
+import Header from './assets/components/Header/Header.jsx';
+import ProductLists from './assets/components/ProductsList/ProductsList.jsx';
+import Navigation from './assets/components/Navigation/Navigation.jsx';
+import ProductFilters from './assets/components/Filters/ProductFilters.jsx';
+import { useState } from 'react';
+import { getAllProducts } from './assets/data/products.js';
 
 function App() {
+  const prodotti = getAllProducts();
+  const [filtroCategory, setFiltroCategory] = useState('')
 
+
+  // Applica tutti i filtri ai giochi disponibili
+  const prodottiFiltrati = prodotti.filter(prodotto => !filtroCategory || prodotto.category === filtroCategory)
     return (
     <>
-      <h1>ciao</h1>
-     
+      <Header products={prodotti}></Header>
+      <Navigation activeTab={activeTab} setActiveTab={setActiveTab}/>
+      <GameFilters 
+        prodotti={prodotti}
+        filtroCategory={setFiltroCategory}
+      />
+      <div className="filtered-games">
+        Risultati: {prodottiFiltrati.length} / {prodotti.length}
+      </div>
+      {
+        prodottiFiltrati.length > 0 ? (
+          <GameList prodotti={giochiFiltrati}/>
+        ) : (
+          <div className="no-games">
+            <span className="emoji">emoji</span>
+            Nessun gioco trovato, prova a cambiare i filtri o la ricerca
+          </div>
+        )
+      }
     </>
   )
 }
